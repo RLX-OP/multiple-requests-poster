71468ae4bdc50b168ce823c8811241ac7ee346ff
RLX <85926055+RLX-OP@users.noreply.github.com>
2022-02-14 20:38:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # -- Import Packages -- #
 import requests
-import os
 import time
 import pyfiglet
 import json
 
 # -- Setup -- #
-#pyfiglet('RLX \n REQUESTS \n POSTER')
-urlfile = open('config.json', 'read')
+print(pyfiglet.figlet_format('RLX \n REQUESTS \n POSTER'))
+urlfile = open('config.json', mode = 'r')
 url = json.load(urlfile)
-proxyfile = open('proxies.txt', 'read')
+proxyfile = open('proxies.txt', mode = 'r')
 
 # -- Proxy, Url And Requests Setup Here -- #
 for link in url:
